1c538499d66c2038bf70d0958841a65cafc068de
ChenYing Kuo <evshary@gmail.com>
2020-11-15 16:49:03 +0800

    Add QoS.

--- 10.QoS/README.md
+++ 10.QoS/README.md
@@ -1,7 +1,22 @@
 # Introduction
 
+The most important feature in ROS 2 is QoS. The QoS in ROS 2 is based on the one in DDS.
+
+There are 6 QoS in ROS 2:
+
+* Reliability: Ensure the data reliability (reliable / best effort)
+* Durability: Provide history data to late-joiner or not (volatile / transient_local)
+* History: Decide how old the data we provide (depth)
+* Lifespan: Data will expire while exceeding its life time
+* Liveliness: Ensure the existence of other entity
+* Deadline: Alarm if data doesn't reach in time
+
+These 6 QoS are independent with each other.
+
 # Reference
 
 * [Design: QoS](https://design.ros2.org/articles/qos.html)
 * [About Quality of Service Settings](https://index.ros.org/doc/ros2/Concepts/About-Quality-of-Service-Settings/)
 * [Demo Code](https://github.com/ros2/demos/tree/master/quality_of_service_demo)
+* [C++ API for QoS](http://docs.ros2.org/latest/api/rclcpp/classrclcpp_1_1QoS.html)
+* [Python API for QoS](http://docs.ros2.org/latest/api/rclpy/api/qos.html)
---
+++ 10.QoS/ros2_ws/src/cpp_qos_demo/src/pub.cpp
@@ -0,0 +1,68 @@
+#include <chrono>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+* member function as a callback from the timer. */
+
+class MinimalPublisher : public rclcpp::Node
+{
+  public:
+    MinimalPublisher()
+    : Node("minimal_publisher"), count_(0)
+    {
+      // Setting QoS
+      rclcpp::QoS qos_settings(10); // history depth is 10
+#define QOS_HISTORY      1
+#define QOS_RELIABILITY  1
+#define QOS_DURABILITY   1
+#if QOS_HISTORY
+      // History
+      qos_settings.keep_last(10);
+      qos_settings.keep_all();
+#endif
+#if QOS_RELIABILITY
+      qos_settings.reliable();
+      qos_settings.best_effort();
+#endif
+#if QOS_DURABILITY
+      qos_settings.durability_volatile();
+      qos_settings.transient_local();
+#endif
+#if QOS_DEADLINE
+      qos_settings.deadline(std::chrono::milliseconds(2000));
+#endif
+#if QOS_LIFESPAN
+#endif
+#if QOS_LIVELINESS
+#endif
+
+      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", qos_settings);
+      timer_ = this->create_wall_timer(
+      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    }
+
+  private:
+    void timer_callback()
+    {
+      auto message = std_msgs::msg::String();
+      message.data = "Hello, world! " + std::to_string(count_++);
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      publisher_->publish(message);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
